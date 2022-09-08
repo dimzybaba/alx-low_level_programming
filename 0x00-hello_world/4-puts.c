4347349a6b5851b9c1f886adf4b1584ffc30df25
dimzybaba <dimzybaba@gmail.com>
2022-09-08 16:08:43 -0700

    real

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * *main - printing
+ *
+ * *
+ *
+ * *Return Always 0 (Sucessful)
+ *
+ * */
+
+int main(void)
+
+{
+
+	        Puts("/"Programming is like building a multilingual puzzle");
+
+		        return (0);
+
+}
