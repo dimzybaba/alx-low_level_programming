e989b0bf4406264747f0bbc19e6bfc3fff983603
dimzybaba <dimzybaba@gmail.com>
2022-09-11 00:32:47 -0700

    read

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,17 +5,11 @@
 #include <stdio.h>
 
 /**
-
 * main - print if the number is postive, zero, or negative
-
 *
-
 * Description: using the main function
-
 * this program prints "Programming is positive, zero, or negative
-
 * Return: 0
-
 */
 
 int main(void)
@@ -23,10 +17,7 @@ int main(void)
 {
 
 int n;
-
 int l;
-
-
 
 srand(time(0));
 
