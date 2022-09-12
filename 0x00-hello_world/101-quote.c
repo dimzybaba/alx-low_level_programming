946bcf8161a211ec87628b792a3f9e2e597ee162
dimzybaba <dimzybaba@gmail.com>
2022-09-12 07:38:15 -0700

    real git push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 i#include <stdio.h>
-
 #include <unistd.h>
 
 /**
@@ -8,12 +7,11 @@ i#include <stdio.h>
 * Description: prints Dora's quote
 * Return: Always l (Success0
 */
-
 int main(void)
 
 {
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 
