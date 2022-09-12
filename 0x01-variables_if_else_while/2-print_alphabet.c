039d11bc6546ae14a081710f46e1e19537639a24
dimzybaba <dimzybaba@gmail.com>
2022-09-12 05:26:16 -0700

    real

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,37 +1,24 @@
 #include <stdio.h>
 
-#include <stdlib.h>
-
-#include <time.h>
-
-
-
- /**
- *
- * main - Entry point
- *
- *
- *
- * Return: Always 0 (Success)
- *
- */
-
-
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*
+*/
 
 int main(void)
 
 {
+char love;
 
-		char love;
+for (love = 'a'; love <= 'z'; love++)
+{
+putchar(love);
+}
 
+putchar('\n');
 
+return (0);
 
-			for (love = 'a'; love <= 'z'; love++)
-
-						putchar(love);
-
-				putchar('\n');
-
-					return (0);
-
-
+}
