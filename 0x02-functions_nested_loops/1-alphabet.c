b173fbd0b67de8cf69e0716c88b16d417a3b90a6
dimzybaba <dimzybaba@gmail.com>
2022-09-13 14:00:41 -0700

    real

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,24 +4,16 @@
 * print_alphabet - function to print abc
 *
 * Return: 0
-*
 */
-
 void print_alphabet(void)
+
 {
+	char c;
 
-		char c;
+	for (c = 'a'; c <= 'z'; c++)
 
-
-
-			for (c = 'a'; c <= 'z'; c++)
-
-					{
-
-								_putchar(c);
-
-									}
-
-			_putchar('\n');
-
+	{
+			_putchar(c);
+	}
+_putchar('\n');
 }
