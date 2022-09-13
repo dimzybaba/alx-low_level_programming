3254f5ac36fcbf9862347cef9a548127ee653fa2
dimzybaba <dimzybaba@gmail.com>
2022-09-13 13:03:07 -0700

    real

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - Entry point
-* Description: 'the program's description'
-* Return: Always 0 (Success)
+* main - Entry
+* Return: always 0
 */
+
 int main(void)
-
 {
-
-	puts("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
